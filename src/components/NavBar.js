6a10c90fa46a736fcef160ec615d8b131173ce5c
CintiaBozzani <cintia.d.bozzani@gmail.com>
2022-10-05 23:49:59 -0300

    add brand, libreria y cartWidget

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,6 @@
 import React from "react"
+import Logo from '../image/logo.png'
+import CartWidget from "./CartWidget"
 import "../styles/NavBar.css"
 
 const NavBar = () => {
@@ -6,10 +8,16 @@ const NavBar = () => {
     return (
         <nav className="nav flexcenter">
             <div className="nav-container-button">
+                <div className="logo-container">
+                    <h3 className="brand">Black Cave</h3>
+                    <img className="logo" src={Logo} alt="Logo Marca" />
+                </div>
+
                 <button className="nav-button">Home</button>
                 <button className="nav-button">Catalogo</button>
                 <button className="nav-button">Accesorios</button>
                 <button className="nav-button">Contacto</button>
+                <CartWidget />
             </div>
         </nav>
     )
