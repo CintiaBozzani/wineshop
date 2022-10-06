1b8ef55aea26abaf078b7da3f329131e24dea267
CintiaBozzani <cintia.d.bozzani@gmail.com>
2022-10-05 23:53:03 -0300

    Primer preentrega Bozzani

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
 
                 <button className="nav-button">Home</button>
                 <button className="nav-button">Catalogo</button>
-                <button className="nav-button">Accesorios</button>
+                <button className="nav-button">Accesorios</button>  
                 <button className="nav-button">Contacto</button>
                 <CartWidget />
             </div>
